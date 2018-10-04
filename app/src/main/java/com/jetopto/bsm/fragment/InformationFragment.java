9664749a7fb0ff33e80802bf137385bf6e1d4d1b
SheldonChang <sheldon.chang@jet-opto.com.tw>
2018-10-04 10:15:25 +0800

    Change as following
    1. Information page implementation.

--- app/src/main/java/com/jetopto/bsm/fragment/InformationFragment.java
+++ app/src/main/java/com/jetopto/bsm/fragment/InformationFragment.java
@@ -1,5 +1,8 @@
 package com.jetopto.bsm.fragment;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,15 +11,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jetopto.bsm.R;
 
 public class InformationFragment extends Fragment {
+
     private static final String TAG = InformationFragment.class.getSimpleName();
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         Log.i(TAG, "onCreateView");
         View view = inflater.inflate(R.layout.fragment_information, container, false);
         initView(view);
@@ -24,8 +30,44 @@ public class InformationFragment extends Fragment {
     }
 
     private void initView(View view) {
-
-
-
+        TextView appTextView = view.findViewById(R.id.app_ver);
+        TextView bsmTextView = view.findViewById(R.id.bsm_ver);
+        appTextView.setText(getString(R.string.label_app_version, getDisplayVersion(getContext())));
+        bsmTextView.setText(getString(R.string.label_bsm_version, getDisplayVersion(getContext())));
     }
+
+    private String getDisplayVersion(Context context) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getVersionName(context));
+        sb.append(" - ");
+        sb.append(getVersionCode(context));
+        return sb.toString();
+    }
+
+    private String getVersionCode(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionCode = "";
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = packageInfo.versionCode + "";
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    private String getVersionName(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionName = "";
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionName;
+    }
+
 }
